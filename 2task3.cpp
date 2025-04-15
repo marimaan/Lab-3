53866e599edce9516c2aa20df8911dcefe77a8e0
Maria Antonova <maria.antonova@metropolia.fi>
2025-04-15 14:05:03 +0300

    Add 2task3.cpp

--- 2task3.cpp
+++ 2task3.cpp
@@ -7,51 +7,50 @@ int main(){
     float num1, num2, result;
     char operation;
 
-    //Small introduction and instructions
-    cout<<"Welcome to the Basic Operations page!\nHere, you will choose ";
-    cout<<"which operations to perform using two numbers you inputted."<<endl;
-    cout<<"Here is a list of operations:\n+ = Addition.\n- = Subtraction."<<endl;
-    cout<<"* = Multiplication.\n/ = Division."<<endl;
+    //Introduction
+    cout<<"Hellol! I am your small calculator."<<endl;
+    cout<<"I can do simple math operations:\n+ Addition.\n- Subtraction."<<endl;
+    cout<<"* Multiplication.\n/ Division."<<endl;
 
     //Do-While loop allows for continuous input
     do{
-        cout<<"\nPlease type in one number: ";
+        cout<<"\nGive me the first number: ";
         cin>>num1;
 
-        cout<<"Now type in another number: ";
+        cout<<"Now type the second number: ";
         cin>>num2;
 
-        cout<<"Finally, choose the operation you'd like to perform: ";
+        cout<<"Choose the operation for your numbers: ";
         cin>>operation;
 
-        //Switch depending on the type of operation to perform with default case to exit
+        //Use a switch statement to handle different operation types, with a default case to exit the program
         switch(operation){
             case '+': {
                 result = num1 + num2;
-                cout<<"You chose addition. Number "<<num1<<" plus number "<<num2;
+                cout<<"Here is the addition answer. Number "<<num1<<" plus number "<<num2;
                 cout<<" is equal to: "<<result<<endl;
                 break;
             }
             case '-':{
                 result = num1 - num2;
-                cout<<"You chose subtraction. Number "<<num1<<" minus number "<<num2;
+                cout<<"Here is the subtraction answer. Number "<<num1<<" minus number "<<num2;
                 cout<<" is equal to: "<<result<<endl;
                 break;
             }
             case '*':{
                 result = num1 * num2;
-                cout<<"You chose multiplication. Number "<<num1<<" multiplied by";
+                cout<<"Here is the multiplication answer. Number "<<num1<<" multiplied by";
                 cout<<" number "<<num2<<" is equal to: "<<result<<endl;
                 break;
             }
             case '/':{
                 result = num1 / num2;
-                cout<<"You chose division. Number "<<num1<<" divided by number "<<num2;
+                cout<<"Here is the division answer. Number "<<num1<<" divided by number "<<num2;
                 cout<<" is equal to: "<<result<<endl;
                 break;
             }
             default:{
-                cout<<"Your choice is invalid. Exiting the program";
+                cout<<"Invalid choice. Exiting the program";
                 break;
             }
         }
