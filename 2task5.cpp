9df6e8a864b52bf9953c466e14dc4fb534e7448c
Maria Antonova <maria.antonova@metropolia.fi>
2025-04-16 13:48:55 +0300

    Add 2task5.cpp

---
+++ 2task5.cpp
@@ -0,0 +1,32 @@
+#include <iostream> 
+using namespace std;
+
+int main(){
+    int arr[]={2,4,6,8,10,12,14,16}; //Initial array
+
+   //The following calculations set the size of array 2 to match the size of the original array
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int arr2[size];
+
+    int* ptr = arr; //Pointer integer to the original array
+
+    cout<<"This is the original array: ";
+    
+    //This for-loop prints each element in the original array
+    for (int i=0; i<size; i++){
+        cout<<*(ptr+i)<<" ";
+    }
+
+    // The second for loop copies elements from array 1 into the new array in reverse order
+    for(int i=0; i<size; i++){
+        arr2[size-1-i]=*(ptr+i);
+    }
+
+    cout<<"\nThis is the new array: ";
+
+    //Printing the new array elements
+    for(int i=0; i<size; i++){
+        cout<<arr2[i]<<" ";
+    }
+    return 0;
+}
